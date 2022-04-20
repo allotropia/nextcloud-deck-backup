893d970e06d4e3d7c8b277dc2c1b9dffa9af07fb
Thorsten Behrens <thorsten.behrens@allotropia.de>
2022-04-20 17:21:19 +0200

    Experimental CSV export, to be used for Redmine
    Close enough to feed Redmine importers a la
    https://github.com/aspiresoftware/redmine-import-tasks

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from utils import DeckDownloader, DeckSender
-import json
 import os
 import argparse
+import csv
 
-FILE_NAME = 'nextcloud-decks.json'
+FILE_NAME = 'nextcloud-decks.csv'
 
 parser = argparse.ArgumentParser()
 
@@ -24,7 +24,26 @@ if(mode == 'backup' or mode == None):
     data = dd.fetchBoards()
 
     with open(os.path.join(args.directory, FILE_NAME), 'w') as outfile:
-        json.dump(data, outfile, indent=4)
+        fieldnames = ["#","Project","Tracker","Parent task","Parent task subject",
+                      "Status","Priority","Subject","Description","Author","Assignee","Updated",
+                      "Category","Sprint/Milestone","Start date","Due date",
+                      "Estimated time","Total estimated time","Spent time","Total spent time",
+                      "% Done","Created","Closed","Last updated by","Related issues","Files",
+                      "Checklist","PSP-Element","Private","Story points","Taskboard position",
+                      "Company","Contact person"]
+        csv_out = csv.DictWriter(outfile, fieldnames=fieldnames)
+        csv_out.writeheader()
+        for deck in data:
+            for stack in deck["stacks"]:
+                if "cards" in stack:
+                    for card in stack["cards"]:
+                        csv_out.writerow({'#': card["id"],
+                                          'Project': deck["title"],
+                                          'Status': stack["title"],
+                                          'Subject': card["title"],
+                                          'Description': card["description"],
+                                          'Updated': card["lastModified"],
+                                          'Due date': card["duedate"]})
 
     for warn in dd.getAllWarnings():
         print(warn)
